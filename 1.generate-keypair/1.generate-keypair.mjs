24c19faf044e2278664489aacf1b9d0a68356141
zhengjunxiang <zhengjunxiang@hzrcb.com>
2025-02-08 16:27:18 +0800

    update

--- 1.generate-keypair/1.generate-keypair.mjs
+++ 1.generate-keypair/1.generate-keypair.mjs
@@ -5,6 +5,7 @@ import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
 
 const keypair = Keypair.generate();
 
+console.log(`The is keypair: `, keypair);
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
 console.log(`The secret key is: `, base58.encode(keypair.secretKey));
@@ -13,7 +14,16 @@ console.log(`✅ Finished!`);
 
 const keypair1 = getKeypairFromEnvironment("SECRET_KEY");
 
-console.log(`The secret key is: `, keypair1.secretKey);
+console.log(`The secret key is: `, base58.encode(keypair1.secretKey));
+
+const userKeypair2 =  Keypair.fromSecretKey(keypair1.secretKey)
+
+console.log(userKeypair2)
+// 转换为Base58编码
+console.log(`The public key is:`,userKeypair2.publicKey.toBase58())
+// 处理私钥的打印格式
+console.log(`The secret key is:`,base58.encode(userKeypair2.secretKey))
+
 
 console.log(
   `✅ Finished! We've loaded our secret key securely, using an env file!`
